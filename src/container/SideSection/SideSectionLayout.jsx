3b9db3915a498b837786a0989e1a4462cbd7b113
Saurabh-Personal <saurabhgarg.iitbhu@gmail.com>
2024-04-09 20:00:38 +0530

    side-section ui improved

--- src/container/SideSection/SideSectionLayout.jsx
+++ src/container/SideSection/SideSectionLayout.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './SideSectionLayout.scss';
 import Button from '../../components/Button/Button';
 import { CommonUtils } from '../../utils/commonfunctions/commonfunctions';
+// import { ReactComponent as ChangePasswordIcon } from '../../assets/icons/'; // Using SVGR
 
 const SideSectionLayout = ({ open, sideSectionShowHandler }) => {
     if (!open) return null;
@@ -11,28 +12,31 @@ const SideSectionLayout = ({ open, sideSectionShowHandler }) => {
             <aside className='side-section-layout'>
                 <div className='side-section-container'>
                     <div className='side-logo-and-web-name'>
-                        <div className='side-logo'></div>
+                        {/* <div className='side-logo'></div> */}
                         <div className='side-web-name'>Aline Patient Manager</div>
                     </div>
-                    <nav className='side-links'>
-                        <ul>
-                            <li className='side-patients-link'>
-                                <Link to={'/home'} onClick={() => sideSectionShowHandler()}>
-                                    Patient Details
-                                </Link>
-                            </li>
-                            <li className='admin-link'>
-                                <Link to={'/users'} onClick={() => sideSectionShowHandler()}>
-                                    Admin
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div className='side-links'>
+                        <nav>
+                            <ul>
+                                <li className='side-patients-link'>
+                                    <Link to={'/home'} onClick={() => sideSectionShowHandler()}>
+                                        Patient Details
+                                    </Link>
+                                </li>
+                                <li className='admin-link'>
+                                    <Link to={'/users'} onClick={() => sideSectionShowHandler()}>
+                                        Admin
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                     <div className='side-logout-container'>
                         <div className='side-email'>saurabhgarg.iitbhu@gmail.com</div>
                         <Button
                             onClickCallBk={CommonUtils.logout.bind(CommonUtils)}
                             title={'Logout'}
+                            type='primary'
                         />
                     </div>
                 </div>
