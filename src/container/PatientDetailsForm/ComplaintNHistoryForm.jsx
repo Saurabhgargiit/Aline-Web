249e9bdcb834cb7d11f90eb6137d0f5352f86057
Saurabh-Personal <saurabhgarg.iitbhu@gmail.com>
2024-05-20 01:21:44 +0530

    Checkbox custom design

--- src/container/PatientDetailsForm/ComplaintNHistoryForm.jsx
+++ src/container/PatientDetailsForm/ComplaintNHistoryForm.jsx
@@ -18,35 +18,47 @@ function ComplaintNHistoryForm() {
                     <span>~~~~~~~~~~~~~~~~~~~~Previous Dental History~~~~~~~~~~~~~~~~~~~~</span>
                 </div>
                 <div className='patient-detials-input-fields'>
-                    <div className='checkbox-container'>
-                        <label htmlFor='crown-bridges'>Crown/Bridges</label>
-                        <input id='crown-bridges' type='checkbox' className='checkbox' />
-                    </div>
+                    <label htmlFor='crown-bridges' className='checkbox-container'>
+                        Crown/Bridges
+                        <input id='crown-bridges' type='checkbox' />
+                        <span className='checkbox'></span>
+                    </label>
                     <label htmlFor='crown-bridges-details'>If Yes, Details</label>
                     <input id='crown-bridges-details' type='text' disabled />
                 </div>
                 <div className='patient-detials-input-fields'>
-                    <label htmlFor='implants'>Implants</label>
-                    <input id='implants' type='checkbox' />
+                    <label htmlFor='implants' className='checkbox-container'>
+                        Implants
+                        <input id='implants' type='checkbox' />
+                        <span className='checkbox'></span>
+                    </label>
                     <label htmlFor='implants-details'>If Yes, Details</label>
                     <input id='implants-details' type='text' disabled />
                 </div>
                 <div className='patient-detials-input-fields'>
-                    <label htmlFor='veneers'>Veneers</label>
-                    <input id='veneers' type='checkbox' />
+                    <label htmlFor='veneers' className='checkbox-container'>
+                        Veneers
+                        <input id='veneers' type='checkbox' />
+                        <span className='checkbox'></span>
+                    </label>
                     <label htmlFor='veneers-details'>If Yes, Details</label>
                     <input id='veneers-details' type='text' disabled />
                 </div>
-                Others
                 <div className='patient-detials-input-fields'>
-                    <label htmlFor='previous-treat'>Previous Orthodontic Treatment</label>
-                    <input id='previous-treat' type='checkbox' />
+                    <label htmlFor='previous-treat' className='checkbox-container'>
+                        Previous Orthodontic Treatment
+                        <input id='previous-treat' type='checkbox' />
+                        <span className='checkbox'></span>
+                    </label>
                     <label htmlFor='previous-treat-details'>If Yes, Details</label>
                     <input id='previous-treat-details' type='text' disabled />
                 </div>
                 <div className='patient-detials-input-fields'>
-                    <label htmlFor='composites'>Composites/Buildup</label>
-                    <input id='composites' type='checkbox' />
+                    <label htmlFor='composites' className='checkbox-container'>
+                        Composites/Buildup
+                        <input id='composites' type='checkbox' />
+                        <span className='checkbox'></span>
+                    </label>
                     <label htmlFor='composites-details'>If Yes, Details</label>
                     <input id='composites-details' type='text' disabled />
                 </div>
