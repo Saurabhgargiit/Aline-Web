61ed4491ddbba22d0f2958882c9b8b0f3aeb32b9
Saurabh-Personal <saurabhgarg.iitbhu@gmail.com>
2024-05-22 21:33:25 +0530

    side patient info component UI completion

--- src/container/PatientDetailsLayout/PatientInfoCenter.jsx
+++ src/container/PatientDetailsLayout/PatientInfoCenter.jsx
@@ -1,41 +1,45 @@
-import React, { memo } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './PatientInfoCenter.scss';
-import Button from '../../components/Button/Button';
-import { CommonUtils } from '../../utils/commonfunctions/commonfunctions';
 
 const PatientInfoCenter = ({}) => {
+    const infoObj = {
+        patientDetails: 'Saurabh Garg',
+        doctorDetails: 'Yash Gupta',
+        clinicDetails: 'Saurabh Garg Clinic private Limited',
+    };
+
+    const img = 'https://d2rdbjk9w0dffy.cloudfront.net/assets/anonymous-user.jpeg';
+
     return (
         <div className='displayFlex'>
             <aside className='right-section-layout'>
                 <div className='right-section-container'>
-                    <div className='side-logo-and-web-name'>
+                    <div className='right-profile-photo'>
+                        <div className='img-container'>
+                            <img
+                                src={
+                                    img ??
+                                    'https://d2rdbjk9w0dffy.cloudfront.net/assets/anonymous-user.jpeg'
+                                }
+                                alt={'Patient Photo'}
+                            ></img>
+                        </div>
                         {/* <div className='side-logo'></div> */}
-                        <div className='side-web-name'>Aline Patient Manager</div>
+                        <div className='side-web-name'></div>
                     </div>
-                    <div className='side-links'>
-                        <nav>
-                            <ul>
-                                <li className='side-patients-link'>
-                                    <Link to={'/home'} onClick={() => {}}>
-                                        Patient Details
-                                    </Link>
-                                </li>
-                                <li className='admin-link'>
-                                    <Link to={'/users'} onClick={() => {}}>
-                                        Admin
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div className='side-logout-container'>
-                        <div className='side-email'>{'email'}</div>
-                        <Button
-                            onClickCallBk={CommonUtils.logout.bind(CommonUtils)}
-                            title={'Logout'}
-                            type='primary'
-                        />
+                    <div className='details-container'>
+                        {Object.entries({
+                            patient: 'Patient',
+                            doctor: 'Doctor',
+                            clinic: 'Clinic',
+                        }).map(([key, label]) => {
+                            return (
+                                <div className='info-div'>
+                                    <span className='info-heading'>{label + ':'}</span>
+                                    <span className='info-detail'>{infoObj[key + 'Details']}</span>
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </aside>
@@ -43,4 +47,4 @@ const PatientInfoCenter = ({}) => {
     );
 };
 
-export default memo(PatientInfoCenter);
+export default PatientInfoCenter;
