9a715c4e68993b57469f7ada4c94a34725d4368c
Saurabh Garg <saurabhgarg.iitbhu@gmail.com>
2024-11-03 22:57:15 +0530

    env files

--- src/store/actions/loginaction.js
+++ src/store/actions/loginaction.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 import { CommonUtils } from '../../utils/commonfunctions/commonfunctions';
 
 const userUpdate = (userData) => {
-    return {
-        type: actionTypes.UPDATE_USER,
-        data: userData,
-    };
+  return {
+    type: actionTypes.UPDATE_USER,
+    data: userData,
+  };
 };
 
 //isLoggedIn --> true/false whether user is loggedIn
@@ -15,44 +15,44 @@ const userUpdate = (userData) => {
 //fromLogin --> false  --> if action is dispatched from app
 
 export const loginAction = (isLoggedIn, fromLogin, loginInfo = {}) => {
-    if (isLoggedIn && fromLogin) {
-        CommonUtils.saveTokens(loginInfo?.data);
-    }
-    const loggedInData = {
-        isLoggedIn: isLoggedIn,
-        loginErr: !isLoggedIn && fromLogin ? loginInfo : '',
-    };
-    return {
-        type: actionTypes.LOGIN,
-        loggedInData: loggedInData,
-    };
+  if (isLoggedIn && fromLogin) {
+    CommonUtils.saveTokens(loginInfo?.data);
+  }
+  const loggedInData = {
+    isLoggedIn: isLoggedIn,
+    loginErr: !isLoggedIn && fromLogin ? loginInfo : '',
+  };
+  return {
+    type: actionTypes.LOGIN,
+    loggedInData: loggedInData,
+  };
 };
 
 export const getLoginData = (url_path, data) => {
-    return (dispatch) => {
-        const path = ApiRelativePaths[url_path];
+  return (dispatch) => {
+    const path = ApiRelativePaths[url_path];
 
-        axios.defaults.baseURL = 'http://localhost:3001';
-        axios
-            .post(path, data, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            .then((res) => {
-                console.log(res);
-                const finalRes = {
-                    result: 'success',
-                    data: { ...res.data },
-                };
-                dispatch(loginAction(true, true, finalRes));
-            })
-            .catch((err) => {
-                const finalErr = {
-                    result: 'error',
-                    data: err.response?.data,
-                };
-                dispatch(loginAction(false, true, finalErr));
-            });
-    };
+    axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+    axios
+      .post(path, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        const finalRes = {
+          result: 'success',
+          data: { ...res.data },
+        };
+        dispatch(loginAction(true, true, finalRes));
+      })
+      .catch((err) => {
+        const finalErr = {
+          result: 'error',
+          data: err.response?.data,
+        };
+        dispatch(loginAction(false, true, finalErr));
+      });
+  };
 };
--- src/utils/globalURLs.js
+++ src/utils/globalURLs.js
@@ -1,52 +1,59 @@
-export const _agent = 'localhost:3001';
+export const _agent = process.env.REACT_APP_API_URL;
 
 export class ApiRelativePaths {
-    //LOGIN
-    static LOGIN_PATH = '/api/v1/aline/auth/login';
-    static GET_TOKENS = '/api/v1/aline/auth/refreshToken';
+  //LOGIN
+  static LOGIN_PATH = '/api/v1/aline/auth/login';
+  static GET_TOKENS = '/api/v1/aline/auth/refreshToken';
 
-    //Logout
-    static LOGOUT = '/api/v1/aline/auth/logout';
+  //Logout
+  static LOGOUT = '/api/v1/aline/auth/logout';
 
-    //USER
-    static GET_USER = '/api/v1/aline/user/getSignedInUserInfo';
-    static CREATE_PARENT_USER = '/api/v1/aline/user/createUserWithDetails';
-    static UPDATE_USER = '/api/v1/aline/user/updateUserWithDetails';
-    static PASS_CHANGE_BY_ADMIN = '/api/v1/aline/user/forgotPassword';
-    static GET_ALL_USERS = '/api/v1/aline/user/getAllUsers';
-    static GET_ALL_USERS_WITH_DETAILS = '/api/v1/aline/user/getAllUsersWithDetails';
-    static ADD_EXISTING_DOCTOR_TO_CLINIC =
-        '/api/v1/aline/clinicDoctorRelationship/addExistingDoctorToClinic';
-    static DELETE_USER = '/api/v1/aline/user/deleteUserByID';
+  //USER
+  static GET_USER = '/api/v1/aline/user/getSignedInUserInfo';
+  static CREATE_PARENT_USER = '/api/v1/aline/user/createUserWithDetails';
+  static UPDATE_USER = '/api/v1/aline/user/updateUserWithDetails';
+  static PASS_CHANGE_BY_ADMIN = '/api/v1/aline/user/forgotPassword';
+  static GET_ALL_USERS = '/api/v1/aline/user/getAllUsers';
+  static GET_ALL_USERS_WITH_DETAILS =
+    '/api/v1/aline/user/getAllUsersWithDetails';
+  static ADD_EXISTING_DOCTOR_TO_CLINIC =
+    '/api/v1/aline/clinicDoctorRelationship/addExistingDoctorToClinic';
+  static DELETE_USER = '/api/v1/aline/user/deleteUserByID';
 
-    //Patient Basic Information
-    static CREATE_PATIENT = '/api/v1/aline/patient/createPatient';
-    static GET_ALL_PATIENTS = '/api/v1/aline/patient/getAllPatients';
-    static UPDATE_PATIENT = '/api/v1/aline/patient/updatePatient';
-    static UPDATE_STATUS = '/api/v1/aline/patient/updatePatientStatus';
-    static DELETE_PATIENT = '/api/v1/aline/patient/deletePatient';
+  //Patient Basic Information
+  static CREATE_PATIENT = '/api/v1/aline/patient/createPatient';
+  static GET_ALL_PATIENTS = '/api/v1/aline/patient/getAllPatients';
+  static UPDATE_PATIENT = '/api/v1/aline/patient/updatePatient';
+  static UPDATE_STATUS = '/api/v1/aline/patient/updatePatientStatus';
+  static DELETE_PATIENT = '/api/v1/aline/patient/deletePatient';
 
-    //Patient Details
-    static GET_PATIENT_DETAILS =
-        '/api/v1/aline/patientDentalDetails/getPatientDentalDetailByPatientID';
-    static UPDATE_PATIENT_DETAILS = '/api/v1/aline/patientDentalDetails/updatePatientDentalDetail';
-    static GET_PATIENT_N_USER_INFO = '/api/v1/aline/patient/getUserDetailsForPatientID';
+  //Patient Details
+  static GET_PATIENT_DETAILS =
+    '/api/v1/aline/patientDentalDetails/getPatientDentalDetailByPatientID';
+  static UPDATE_PATIENT_DETAILS =
+    '/api/v1/aline/patientDentalDetails/updatePatientDentalDetail';
+  static GET_PATIENT_N_USER_INFO =
+    '/api/v1/aline/patient/getUserDetailsForPatientID';
 
-    //Photos & Scans
-    static GET_PHOTOS_SCANS_URLS =
-        '/api/v1/aline/patientPhotoScans/getPatientPhotoScansByPatientID';
-    static ADD_URLS_TO_DATABASE = '/api/v1/aline/patientPhotoScans/updatePatientPhotoScans';
+  //Photos & Scans
+  static GET_PHOTOS_SCANS_URLS =
+    '/api/v1/aline/patientPhotoScans/getPatientPhotoScansByPatientID';
+  static ADD_URLS_TO_DATABASE =
+    '/api/v1/aline/patientPhotoScans/updatePatientPhotoScans';
 
-    //Reboots
-    static GET_REBOOT_IDS = '/api/v1/aline/dentalDetailsMapping/getAllRebootIds';
-    static CREATE_REBOOT = '/api/v1/aline/dentalDetailsMapping/createReboot'
+  //Reboots
+  static GET_REBOOT_IDS = '/api/v1/aline/dentalDetailsMapping/getAllRebootIds';
+  static CREATE_REBOOT = '/api/v1/aline/dentalDetailsMapping/createReboot';
 
-    //Treatment Plans
-    static GET_TREATMENTPLAN_MAPPING = '/api/v1/aline/dentalDetailsMapping/getPlanMapping';
-    static CREATE_TREATMENT_PLAN= '/api/v1/aline/treatmentPlan/saveDraft'; //POST 
-    static UPDATE_TREATMENT_PLAN= '/api/v1/aline/treatmentPlan/updateDraft'; //PULL
-    static GET_TREATMENT_PLAN_DETAILS = '/api/v1/aline/treatmentPlan/getPlan';
-    static SHARE_PLAN_WITH_DOCTOR= '/api/v1/aline/treatmentPlan/sendPlanModification';
-    static REQUEST_FOR_MODIFICATION = '/api/v1/aline/treatmentPlan/planRequestModification';
-    static APPROVE_PLAN ='/api/v1/aline/treatmentPlan/approvePlan';
+  //Treatment Plans
+  static GET_TREATMENTPLAN_MAPPING =
+    '/api/v1/aline/dentalDetailsMapping/getPlanMapping';
+  static CREATE_TREATMENT_PLAN = '/api/v1/aline/treatmentPlan/saveDraft'; //POST
+  static UPDATE_TREATMENT_PLAN = '/api/v1/aline/treatmentPlan/updateDraft'; //PULL
+  static GET_TREATMENT_PLAN_DETAILS = '/api/v1/aline/treatmentPlan/getPlan';
+  static SHARE_PLAN_WITH_DOCTOR =
+    '/api/v1/aline/treatmentPlan/sendPlanModification';
+  static REQUEST_FOR_MODIFICATION =
+    '/api/v1/aline/treatmentPlan/planRequestModification';
+  static APPROVE_PLAN = '/api/v1/aline/treatmentPlan/approvePlan';
 }
